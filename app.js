633d394ddaa3c2519fe854bc252ec1bd1dc96a7b
Surachet Sangasaeng <basone01@hotmail.com>
2017-05-01 09:01:52 +0700

    map

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-
+var readProvincesFile = fs.readFileSync("provinces.json");
+var provinces = JSON.parse(readProvincesFile);
+var readPlaceFile = fs.readFileSync("place.json");
+var places = JSON.parse(readPlaceFile);
+console.log(places[0]);
 
 app.get("/", (req,res)=>{
 	res.render("index",{
+		provinces:provinces,
 		pic:[
 			{
 				link:"/search?place=เชียงใหม่",
@@ -78,30 +82,31 @@ app.get("/listsearch",(req,res)=>{
 });
 
 app.get("/search",(req,res)=>{
+	res.render('search',{places:places});
+	// console.log(req.query);
+  // if (req.query.place=='') {
+  //   res.render('search',{search:false});
+  // }else {
+  //   var data = fs.readFileSync("place.json");
+  //   var place = JSON.parse(data);
+  //   var jsonres = [];
+  //   for (var i = 0; i < place.length; i++) {
+  //     if(place[i].name.includes(req.query.place)){
+  //       jsonres.push(place[i]);
+  //     }else if (place[i].province.includes(req.query.place)) {
+  //       jsonres.push(place[i]);
+  //     }
+  //   }
+	// 	if(jsonres.length>0){
+	// 		res.render('search',{
+	// 			search:true,
+	// 			datas:jsonres
+	// 		});
+	// 	}else{
+	// 		res.render('search',{search:false});
+	// 	}
 
-  if (req.query.place=='') {
-    res.render('search',{search:false});
-  }else {
-    var data = fs.readFileSync("place.json");
-    var place = JSON.parse(data);
-    var jsonres = [];
-    for (var i = 0; i < place.length; i++) {
-      if(place[i].name.includes(req.query.place)){
-        jsonres.push(place[i]);
-      }else if (place[i].province.includes(req.query.place)) {
-        jsonres.push(place[i]);
-      }
-    }
-		if(jsonres.length>0){
-			res.render('search',{
-				search:true,
-				datas:jsonres
-			});
-		}else{
-			res.render('search',{search:false});
-		}
-
-  }
+  // }
 
  });
 
